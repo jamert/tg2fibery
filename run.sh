8cf472d5e3055baf6df3342fa21aa292f5e97694
Artem Daineko <dayneko.ab@gmail.com>
2022-04-23 01:13:29 +0300

    Add basic Telegram Bot API mock

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker build --tag tg2fibery:latest .
 docker image prune --force
-docker-compose run --rm dev /bin/bash
+# docker-compose run --rm dev /bin/bash
+docker-compose run --rm test
 docker-compose down
