b2dca0844d1c83651818b7dd3b70910d464ca00d
Artem Daineko <dayneko.ab@gmail.com>
2022-05-19 00:59:57 +0300

    Add --secret option to tg2fibery command

--- tg2fibery.py
+++ tg2fibery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import configparser
 
+import click
 from attr import define
 
 
@@ -9,14 +10,19 @@ class Telegram:
     token: str
 
 
-def get_token():
+def get_token(config_string: str) -> str:
     parser = configparser.ConfigParser()
-    parser.read("secrets.ini", encoding="utf-8")
+    parser.read_string(config_string)
     return parser.get(section="telegram", option="token")
 
 
-def main():
-    print(Telegram(token=get_token()))
+@click.command
+@click.option(
+    "--secret", type=click.Path(exists=True, dir_okay=False), default="secrets.ini"
+)
+def main(secret: str) -> None:
+    with open(secret, "rt") as fd:
+        print(Telegram(token=get_token(fd.read())))
 
 
 if __name__ == "__main__":
